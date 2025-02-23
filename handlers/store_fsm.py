ddd555b3cd330be81ad4a8bf3fd591797259211c
Arturchik07 <arstanbekovartur202@gmail.com>
2025-02-23 18:28:41 +0600

    hwm55

--- handlers/store_fsm.py
+++ handlers/store_fsm.py
@@ -1,7 +1,7 @@
 from aiogram import types, Dispatcher
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher.filters import Text
-from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher import FSMContext  #$#
 
 class StoreFSM(StatesGroup):
     name_product = State()
